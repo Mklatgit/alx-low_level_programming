0f3e07cf6c2d26bead35dcfb52e85c67668a4ec8
Moroke <morokelekhooana@gmail.com>
2023-02-16 13:31:09 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	char me[] = "\"Programing is like building a multilingual puzzle";
+
+	puts(me);
+
+	return (0);
+}
