d926261fdb41110470c31ab62da0ebdebd4a9f12
Moroke <morokelekhooana@gmail.com>
2023-02-18 12:24:56 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cc;
+
+	for (cc = 'a'; cc <= 'z'; cc++)
+	{
+		putchar(cc);
+	}
+	for (cc = 'A'; cc <= 'Z'; cc++)
+	{
+		putchar(cc);
+	}
+	putchar('\n');
+	return (0);
+}
