a8a5a46aeaea104f69c93e9960445de20590ac7e
Moroke <morokelekhooana@gmail.com>
2023-02-18 14:26:28 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: first digit of a 2 digit num is the remainder of num/10
+ *last digit in num mod 10 from task 1
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cc, bb, aa;
+
+	for (cc = 0; cc < 100; cc++)
+	{
+		bb = cc / 10;
+		aa = cc % 10;
+		putchar(aa + '0');
+		putchar(bb + '0');
+		if (cc < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
