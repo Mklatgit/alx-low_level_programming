41bec65e77c376d10378266229da0e3b8e3072f6
Moroke <morokelekhooana@gmail.com>
2023-02-18 14:02:25 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cc;
+
+	for (cc = 0; cc < 10; cc++)
+	{
+		putchar(cc + '0');
+		putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
