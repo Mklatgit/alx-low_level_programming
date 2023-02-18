5f3fa84c1a03c0b1ef0d7b220a3e4f0ee79a15b5
Moroke <morokelekhooana@gmail.com>
2023-02-18 13:15:17 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cc;
+
+	for (cc = 'z'; cc >= 'a'; cc++)
+	{
+		putchar(cc);
+	}
+	putchar('\n');
+	return (0);
+}
