b86d615383797e5c28105e1b2a99eadb049b122a
Moroke <morokelekhooana@gmail.com>
2023-02-18 13:04:15 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cc;
+
+	for (cc = 0; cc < 10; cc++)
+	{
+		putchar(cc);
+	}
+	putchar('\n');
+	return (0);
+}
