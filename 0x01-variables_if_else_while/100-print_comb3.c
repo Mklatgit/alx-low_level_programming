cdecfcb564b1f131bcf41782217ca0008d269de5
Moroke <morokelekhooana@gmail.com>
2023-02-20 08:52:56 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,34 @@
  *
  *Description: first digit of a 2 digit num is the remainder of num/10
  *last digit in num mod 10 from task 1
+ *then try compare output with reverse of num to get unique values
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int cc, bb, aa;
+	int cc, bb, aa, dd = 0, ee, num;
 
-	for (cc = 0; cc < 90; cc++)
+	for (cc = 1; cc < 90; cc++)
 	{
 		bb = cc / 10;
 		aa = cc % 10;
-		putchar(bb + '0');
-		putchar(aa + '0');
-		if (cc < 89)
+		ee = cc;
+		/* Reverse number */
+		while (ee != 0)
 		{
-			putchar(',');
-			putchar(' ');
+			num = ee % 10;
+			dd = dd * 10 + num;
+			ee /= 10;
+		}
+		if (bb != aa)
+		{
+			putchar(bb + '0');
+			putchar(aa + '0');
+			if (cc < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
