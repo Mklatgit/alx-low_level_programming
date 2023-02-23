4fce6670c64fb1ca0d8b21144548e75a2630c3d7
Moroke <morokelekhooana@gmail.com>
2023-02-22 23:09:33 -0800

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,20 +8,14 @@
 int main(void)
 {
 	int a, b = 0, n = 1024;
-
-	printf("List of numbers from 0 to %d that are multiples of 3 and 5 :\n", n);
+	
 	for (a = 0; a < n; a++)
 	{
 		if (a % 3 == 0 || a % 5 == 0)
 		{
-			printf("%d ", a);
-			if (a < n - 1)
-			{
-				printf(",");
-			}
 			b = b + a;
 		}
 	}
-	printf("\nTheir Sum is: %d", b);
+	printf("Sum is: %d\n", b);
 	return (0);
 }
