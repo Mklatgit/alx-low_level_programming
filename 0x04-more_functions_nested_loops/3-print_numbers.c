2289a461a3fe57c2bd0fb2fcf9ced4a5151c041d
Moroke <morokelekhooana@gmail.com>
2023-02-23 16:24:15 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print number 0 to 9
+ *
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + '0');
+	}
+}
