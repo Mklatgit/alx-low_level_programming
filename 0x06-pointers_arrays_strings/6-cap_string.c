927757878d49bc321eeb5cc2ae3717056929d3fe
Mklatgit <morokelekhooana@gmail.com>
2023-03-02 00:41:51 +0200

    Expect the best.Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - changes string from lower to upper cases
+ * @src: input string
+ * Return: n
+ */
+char *cap_string(char *src)
+{
+	int len;
+
+	len = 0;
+	for (len = 0; src[len] != '\0'; len++)
+	{
+		if (src[len - 1] == ' ' || src[len - 1] == '.' || src[len - 1] == '\t' || src[len - 1] == '\n' || len == 0)
+		{
+			if (src[len] >= 'a' && src[len] <= 'z')
+			{
+				src[len] = src[len] - 32;
+			}
+		}
+		else
+		{
+			if (src[len] >= 'A' && src[len] <= 'Z')
+				src[len] = src[len] + 32;
+		}
+			
+		
+	}
+	return (src);
+}
