f26bff3ad4c3af2bb825a4aa01d2703f645565a6
Moroke <morokelekhooana@gmail.com>
2023-03-08 10:51:22 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sprt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns square root of num
+ *@n: input number
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return _sqrt_num(n, 0);
+}
+/**
+ * _sqrt_num - returns square
+ *@x: input number
+ *@y: power
+ * Return: Always 0.
+ */
+int _sqrt_num(int x, int y)
+{
+	int val = x;
+	int counter = 1 + y;
+	int sqroot = 1, output;
+
+	while (sqroot <= val)
+	{
+		counter++;
+		sqroot = counter * counter;
+	}
+	output = counter - 1;
+	return (output);
+}
