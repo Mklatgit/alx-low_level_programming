bcfdd54a060577e7008bcb66e8b5346a1232f4e9
Moroke <morokelekhooana@gmail.com>
2023-03-15 14:57:25 -0700

    concatenate arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, n, j = 0, k = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			k++;
+	}
+	k += ac;
+	s = malloc(sizeof(char) * k + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			s[j] = av[i][n];
+			j++;
+		}
+		if (s[j] == '\0')
+		{
+			s[j++] = '\n';
+		}
+	}
+	return (s);
+}
