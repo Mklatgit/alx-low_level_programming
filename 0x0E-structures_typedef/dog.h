1b9b207c3119d849e68805e8b1381b9435602d0e
Moroke <morokelekhooana@gmail.com>
2023-03-20 13:19:49 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
