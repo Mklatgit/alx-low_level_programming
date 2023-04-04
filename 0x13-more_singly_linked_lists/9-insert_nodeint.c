970157b6f4ae304744f7f767745f5aeb97d1ea2a
Moroke <morokelekhooana@gmail.com>
2023-04-04 12:07:06 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (current->next != NULL && i < idx)
+	while (current != NULL && i < idx)
 	{
 		if (i == idx - 1)
 		{
