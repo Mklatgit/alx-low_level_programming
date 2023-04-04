cecceab04e3a1ccdebb50ca477ab9ffd1adaaa6f
Moroke <morokelekhooana@gmail.com>
2023-04-04 14:00:04 -0700

    Delete at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+int pop_listint(listint_t **head);
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head: list to print
+ * @index: position where to delete
+ *
+ * Return: Always 1 if succeeded -1 if failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *rem_node = *head;
+	listint_t *prev_node = NULL;
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		rem_node = (*head)->next;
+		free(*head);
+		*head = rem_node;
+		return (1);
+	}
+	for (i = 0; i < index - 1; i++)
+	{
+		if (rem_node == NULL || rem_node->next == NULL)
+		{
+			return (-1);
+		}
+		rem_node = rem_node->next;
+	}
+	prev_node = rem_node->next;
+	rem_node->next = prev_node->next;
+	free(prev_node);
+	return (1);
+}
