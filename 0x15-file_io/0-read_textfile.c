290434865d5957cbb6b97da8dee97f9da8697742
Moroke <morokelekhooana@gmail.com>
2023-04-10 10:46:20 -0700

    Task 0- Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a textfile
+ * @filename: the file to read
+ * @letters: number of letters to read
+ *
+ * Return: number of letters to read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	char *p;
+	ssize_t act_bytes;
+	ssize_t reader;
+
+	fd = open(filename, O_RDONLY);
+	if (!fd)
+	{
+		return (0);
+	}
+	p = malloc(sizeof(char) * letters);
+	reader = read(fd, p, letters);
+	act_bytes = write(1, p, reader);
+
+	free(p);
+	close(fd);
+	return (act_bytes);
+}
